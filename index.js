6b132317fd7d193dbe2832cfd22a66a1d07536ad
rithik pathak <43316269+thescarow@users.noreply.github.com>
2024-02-03 20:25:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import bodyParser from "body-parser"
 import cors from "cors"
-// import dotenv from "dotenv
+// import dotenv from "dotenv"
 import path from "path"
 // dotenv.config()
 
